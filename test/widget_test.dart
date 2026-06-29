d42f6d3901b90100a589024d898825673185a975
Razin <razinmohammedpt@gmail.com>
2026-06-29 18:55:55 +0530

    Refactor widget test to remove comments and enhance clarity

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,16 +1,15 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:right_answer/main.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
   testWidgets('App smoke test', (WidgetTester tester) async {
     await tester.pumpWidget(const RightAnswerApp());
-    expect(find.text('RightAnswer'), findsOneWidget);
+    await tester.pump();
+
+    expect(find.byType(RightAnswerApp), findsOneWidget);
+    expect(find.byType(MaterialApp), findsOneWidget);
   });
 }
